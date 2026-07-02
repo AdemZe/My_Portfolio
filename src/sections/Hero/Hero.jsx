33eec949650a645823ff64e017e024dfe4830500
AdemZe <doumadaghrour@gmail.com>
2026-07-02 18:41:04 +0100

    update cv

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -6,7 +6,7 @@ import githubLight from "../../assets/github-light.svg";
 import githubDark from "../../assets/github-dark.svg";
 import linkedinLight from "../../assets/linkedin-light.svg";
 import linkedinDark from "../../assets/linkedin-dark.svg";
-import CV from "../../assets/CV_Adem_Daghrour.pdf";
+import CV from "../../assets/Adem_Daghrour_Cv.pdf";
 import { useTheme } from "../../common/ThemeContext";
 
 function Hero() {
